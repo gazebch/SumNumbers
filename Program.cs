ac9a1397aad8a8bef19b5bf96d15d83e06bab0ae
Gazeba <sipion46@mail.ru>
2022-06-21 18:03:04 +0300

    убрал преобразование (в string) в методе суммирования

--- Program.cs
+++ Program.cs
@@ -5,17 +5,18 @@ var mas = Enumerable.Repeat(0, 20).Select(x => r.Next(1, 150)).ToArray();
 Console.WriteLine("входной массив");
 Console.WriteLine(string.Join(" ", mas.Select(x => x.ToString()).ToArray()));
 
-var v = mas.OrderBy(s => SumDigit(s.ToString()));
+var v = mas.OrderBy(s => SumDigit(s));
 
 Console.WriteLine("отсортированный массив по сумме цифр");
 Console.WriteLine(string.Join(" ", v.Select(x => x.ToString()).ToArray()));
 
-int SumDigit(string dig)
+int SumDigit(int dig)
 {
     var sum = 0;
-    for (var i = 0; i < dig.Length; i++)
+    while (dig != 0)
     {
-        sum += int.Parse(dig[i].ToString());
+        sum += dig % 10;
+        dig /= 10;
     }
     return sum;
 };
